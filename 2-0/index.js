080d5cf67619983b71faa6016fbfc0520eb45155
Jaehyup Shin <tlswoguq@naver.com>
2020-07-11 14:49:51 +0100

    #2-1.1 More Function Fun

---
+++ 2-0/index.js
@@ -0,0 +1,5 @@
+function sayHello(arg1, arg2) {
+    console.log('Hello! ', arg1, " you are ", arg2);
+}
+
+sayHello(jaehyup, programmer);
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 function sayHello(arg1, arg2) {
-    console.log('Hello! ', arg1, " you are ", arg2);
+    //console.log('Hello! ', arg1, " you are ", arg2);
+    console.log(`Hello ${arg1} you are ${arg2}`);
+    return `Hello ${arg1} you are ${arg2}`;
 }
 
-sayHello(jaehyup, programmer);
+const greetJH = sayHello(jaehyup, programmer)
+
+console.log(greetJH)
+
+const calculator = {
+    plus: function(a, b) {
+        return a + b;
+    },
+    minus: function(a, b) {
+        return a - b;
+    },
+    multiple: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    }
+}
+
+const plusResult = calculator.plus(5, 5)
+console.log(plusResult)
