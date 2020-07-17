56c4f1ff2e2fe8bf3a7b260838689fd54ceec83a
shin0343 <shin0343@mail.hongik.ac.kr>
2020-07-17 09:30:55 +0100

    arrange dir

--- 3-0/clock.js
+++ 3-0/clock.js
@@ -6,12 +6,11 @@ function getTime() {
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours <10?`0${hours}`:hours}:${minutes<10? `0${minutes}`:minutes}:${seconds < 10? `0${seconds}` : seconds}`;
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function init() {
     getTime();
-    setInterval(getTime, 1000);
 }
 
 init();
