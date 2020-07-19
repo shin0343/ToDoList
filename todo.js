1a2cde2345234e7a1c8db889c84bbe9cf6899bd4
shin0343 <shin0343@mail.hongik.ac.kr>
2020-07-19 05:18:39 +0100

    #3-5 Making a To Do List part One

--- todo.js
+++ todo.js
@@ -5,14 +5,21 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 const TODOS_LS = 'toDos';
 
 function paintToDo(text) {
-    console.log(text);
+    const dl = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "X";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span); //Set li
+    toDoList.appendChild(li); //Append to ul
 }
 
 function handleSubmit(event) {
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
-    
+    toDoInput.value = "";
 }
 
 function loadToDos() {
