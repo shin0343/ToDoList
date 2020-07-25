9e7b2723d8944f25b4aec6f7c126d4dd9f7db3d7
JB <shin0343@mail.hongik.ac.kr>
2020-07-25 01:06:08 +0100

    Edit autopush.sh

--- autopush.sh
+++ autopush.sh
@@ -6,7 +6,7 @@ git add *
 echo -e "\033[43;31mGit Add is completed!!\033[0m"
 
 git commit -m "${commitContents}"
-echo -e "\033[43;31mEngineer\033[0m"
+echo -e "\033[43;31mGit Commit is completed!! = ${commitContents}\033[0m"
 
 git push origin master
-echo -e "\033[43;31mEngineer\033[0m"
+echo -e "\033[43;31mGit Push is completed!!\033[0m"
