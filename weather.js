a2ccd0c6fa20d1e5a12935eb17f9555519ea0e9d
JB <shin0343@mail.hongik.ac.kr>
2020-08-03 01:47:40 +0100

    Change UI 2008030947

--- weather.js
+++ weather.js
@@ -1,6 +1,58 @@
 const weather = document.querySelector(".js-weather");
 const API_KEY = "d3cdd52b097ee26db148f8b3c29b10bc";
 const COORDS = "coords";
+let locationIcon = document.getElementsByClassName('.js-weather-icon');
+
+const weatherCases = {
+    Rain: {
+        colors: ["#00C6FB", "#005BEA"],
+        title: "Raining",
+        subtitle: "우산 챙기세요",
+        icon: "ios-rainy"
+    },
+    Clear: {
+        colors: ["#FEF253", "#FF7300"],
+        title: "Sunny",
+        subtitle: "날이 좋아요",
+        icon: "ios-sunny"
+    },
+    Thunderstorm: {
+        colors: ["#00ECBC", "#007ADF"],
+        title: "Thunderstrom",
+        subtitle: "천둥이쳐요!",
+        icon: "ios-thunderstorm"
+    },
+    Clouds: {
+        colors: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "구름이 꼈어요",
+        icon: "ios-cloudy"
+    },
+    Snow: {
+        colors: ["#7DE2FC", "#B9B6E5"],
+        title: "Snow",
+        subtitle: "눈이와요!",
+        icon: "ios-snow"
+    },
+    Drizzle: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subtitle: "이슬비가 내려요",
+        icon: "ios-rainy-outline"
+    },
+    Haze: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Haze",
+        subtitle: "이슬비가 내려요",
+        icon: "ios-rainy-outline"
+    },
+    Mist: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Mist",
+        subtitle: "이슬비가 내려요",
+        icon: "ios-rainy-outline"
+    }
+}
 
 function getWeather(lat, lng) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
@@ -8,7 +60,10 @@ function getWeather(lat, lng) {
             return response.json();
         })
         .then(function(json) { // keyword 'then' waits 'fetch'
-            //const weatherIcon = json.main.weather[0].icon;
+            console.log(json);
+            const iconCode = json.weather[0].icon;
+            const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+            const mainWeather = json.weather[0].description;
             const temperature = json.main.temp;
             const humid = json.main.humidity;
             const place = json.name;
@@ -16,10 +71,14 @@ function getWeather(lat, lng) {
             ┌───────────┐
             │   <<Today Weather>>
             │  ───────────
-            │   <Temperature> ${temperature} ℃ 
-            │   <Humid> ${humid} % 
+            │   <Current> ${mainWeather}
+            │   <Temperature> ${temperature}℃ 
+            │   <Humid> ${humid}% 
             │   <City> ${place}
             └───────────┘`;
+
+            locationIcon.innerHTML = `<img src="${iconUrl}"/>`;
+            console.log(locationIcon);
         });
 }
 
